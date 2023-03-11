4b7b0c55e53206864df5f0d986250eb856c1e57f
Dan <danielguizar848@gmail.com>
2023-03-10 22:47:14 -0800

    Added some more items

--- src/main/java/net/coderdan/mccourse/block/ModBlocks.java
+++ src/main/java/net/coderdan/mccourse/block/ModBlocks.java
@@ -4,17 +4,22 @@ import net.coderdan.mccourse.MCCourseMod;
 import net.coderdan.mccourse.block.custom.SpeedyBlock;
 import net.coderdan.mccourse.item.ModCreativeModeTab;
 import net.coderdan.mccourse.item.ModItems;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -44,9 +49,40 @@ public class ModBlocks {
     public static final RegistryObject<Block> SPEEDY_BLOCK = registerBlock("speedy_block", ()-> new SpeedyBlock(
             BlockBehaviour.Properties.of(Material.STONE)
                     .strength(4f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB, "tooltip.block.speedy_block");
+
+    public static final RegistryObject<Block> COBALT_STAIRS = registerBlock("cobalt_stairs",
+            ()-> new StairBlock(()-> ModBlocks.COBALT_BLOCK.get().defaultBlockState(),
+            BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(4f)
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
 
+    public static final RegistryObject<Block> COBALT_SLAB = registerBlock("cobalt_slab", ()-> new SlabBlock(
+            BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(4f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
+
+
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab, String tooltipKey){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab, tooltipKey);
+        return toReturn;
+
+    }
+
+
+    public static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab, String tooltipKey){
+        return ModItems.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties().tab(tab)){
+
+            @Override
+            public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+                pTooltip.add(new TranslatableComponent(tooltipKey));
+            }
+        });
+    }
 
 
 
@@ -58,15 +94,18 @@ public class ModBlocks {
 
     }
 
+
+    public static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
+        return ModItems.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+
     private static <T extends Block> RegistryObject<T> registerBlockWithNoItem(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         return toReturn;
 
     }
 
-    public static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
-        return ModItems.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties().tab(tab)));
-    }
 
     public static void register(IEventBus bus){
         BLOCKS.register(bus);
